b3acda43fef8b42f12d4bf709d7a5627af4e1377
pilpod <giacomo.corso@gmail.com>
2021-05-13 21:35:50 +0200

    StoneFactory added and working

--- src/Models/FactoryModels/StoneFactory.php
+++ src/Models/FactoryModels/StoneFactory.php
@@ -5,11 +5,13 @@ namespace App\Models\FactoryModels;
 use App\Contracts\ISelectable;
 use App\Contracts\ISelectableFactory;
 
+use App\Models\Stone;
+
 class StoneFactory implements ISelectableFactory {
 
     public function createObject(): ISelectable
     {
-        
+        return new Stone();
     }
 
 }
--- tests/Models/ObjectsTest.php
+++ tests/Models/ObjectsTest.php
@@ -5,15 +5,19 @@ namespace Tests\Models;
 use PHPUnit\Framework\TestCase;
 
 use App\Models\FactoryModels\ScissorsFactory;
+use App\Models\FactoryModels\StoneFactory;
 use App\Models\Scissors;
+use App\Models\Stone;
 
-class ScissorsTest extends TestCase {
+class ObjectsTest extends TestCase {
     
     private ScissorsFactory $scissorsFactory;
+    private StoneFactory $stoneFactory;
 
     protected function setUp(): void
     {
         $this->scissorsFactory = new ScissorsFactory();
+        $this->stoneFactory = new StoneFactory();
     }
 
     public function test_ScissorsFactory_return_intance_of_Scissors()
@@ -22,6 +26,15 @@ class ScissorsTest extends TestCase {
 
         $this->assertInstanceOf(Scissors::class, $scissors);
     }
+
+    public function test_StoneFactory_return_intance_of_Stone()
+    {
+        $stone = $this->stoneFactory->createObject();
+
+        $this->assertInstanceOf(Stone::class, $stone);
+    }
+
+
 
 }
 
